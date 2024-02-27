413cd4a6100936d6265ece422c2a5e1ae6b94ff3
Felipe Gallego <felipegallego600@gmail.com>
2024-02-27 15:40:27 -0500

    change-model

--- src/Backend/utils/openai.js
+++ src/Backend/utils/openai.js
@@ -49,7 +49,7 @@ const resumen = async (video1,video2)=>{
 
     try {
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
+            model: "gpt-3.5-turbo-0125",
             messages: [{ role: "user", content: prompt }]
         });
         return response.choices[0].message.content
